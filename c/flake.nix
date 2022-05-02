cb17e3b1e76a0751440d7011766f7d11769896f5
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-05-02 18:32:21 +0200

    [c] Added a c template.

---
+++ c/flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixpkgs-unstable;
+    utils.url = github:numtide/flake-utils;
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = with pkgs; mkShell {
+          packages = [
+            clang-tools
+            gnumake
+          ];
+        };
+      });
+}
