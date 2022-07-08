fb36fb46c7f5cabf3907d58ef13705e4bd7c3f5e
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-07-08 16:53:33 +0200

    [python] Added setup stuff.

---
+++ python/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='!NAME!',
+    version='0.1.0',
+    packages=['!NAME!'],
+    entry_points={
+        'console_scripts': [
+            '!NAME! = !NAME!.__main__:main',
+        ],
+    },
+)
