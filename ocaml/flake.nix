301cf529626286cc990fe3e3cb0a8b8be5c3ce1d
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-09-22 09:26:21 +0200

    [ocaml] Patched bug in flake.nix.

--- ocaml/flake.nix
+++ ocaml/flake.nix
@@ -11,7 +11,7 @@
       let
         pkgs = import nixpkgs { inherit system; };
       in {
-        devShell = with pkgs.mkShell; {
+        devShell = pkgs.mkShell {
           buildInputs = with pkgs; [
             ocaml
             dune_3
