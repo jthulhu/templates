2dbdb351f38c3782e07e11651260a37dd566314e
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-09-22 17:59:33 +0200

    [c/{Makefile,flake}] Patched bugs, added install target.

--- c/flake.nix
+++ c/flake.nix
@@ -8,16 +8,29 @@
 
   outputs = { self, nixpkgs, utils }:
     utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; };
+      let
+        inherit (pkgs.stdenv) mkDerivation;
+        pkgs = import nixpkgs { inherit system; };
+        buildInputs = with pkgs; [
+          gcc
+          gnumake
+        ];
       in {
+        defaultPackage = mkDerivation {
+          inherit buildInputs;
+          pname = "!NAME!";
+          version = "0.1.0";
+          src = ./.;
+          makeFlags = [
+            "PREFIX=${placeholder "out"}"
+          ];
+        };
         devShell = with pkgs; mkShell {
           packages = [
             clang-tools
-            gcc
             gdb
-            gnumake
             valgrind
-          ];
+          ] ++ buildInputs;
         };
       });
 }
